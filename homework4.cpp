cf8e00c0e9365a379e37c2019f405a4e3ff8b10d
Pursheld <stompee@bk.ru>
2021-04-20 21:45:20 +0300

    Добавьте файлы проекта.

---
+++ homework4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+    const int size = 2;
+    const int srange = 4;
+    int array[size][srange];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < srange; j++)
+        {
+
+            array[i][j] = i + j;
+
+        }
+
+    }
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < srange; j++)
+        {
+
+            std::cout << array[i][j];
+
+        }
+        std::cout << '\n';
+    }
+
+}

104c76c4bf50e2fbf79c66b0b6d4cc368758dc38
Pursheld <stompee@bk.ru>
2021-04-24 14:36:45 +0300

    change 2

--- homework4.cpp
+++ homework4.cpp
@@ -4,7 +4,7 @@ int main()
 {
     
     const int size = 2;
-    int array[size][size] = { {1,2},{5,3} };
+    int array[size][size] = { {1,2},{1,2} };
     for (int i = 0; i < size; i++)
     {
         
